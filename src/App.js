db8e91e83919b352bc94490bd3a9fe004a9023e3
Jenna <jenna48071@gmail.com>
2022-09-15 20:52:55 -0500

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Axios from "axios";
+import {v4 as uuidv4} from 'uuid'
 import Card from './components/Card'
 
 function App() {
   const [search, setSearch] = useState('');
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
+  const [healthLabels, setHealthLabels] = useState('alcohol-free');
 
   //https://api.edamam.com/search?q=chicken&app_id=1f947df2&app_key=bf2f0ead683c3918124966e995be981c&from=0&to=3&calories=591-722&health=alcohol-free
 
@@ -12,7 +14,7 @@ function App() {
   const APP_ID = '1f947df2'
 
   async function fetchData(){
-    let res = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${API_KEY}&&health=alcohol-free`)
+    let res = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${API_KEY}&health=${healthLabels}`)
     setRecipes(res.data.hits)
     console.log(res.data);
   }
@@ -21,19 +23,36 @@ function App() {
     e.preventDefault();
     fetchData()
   }
+
   
   return (
     <div className="App">
-      <h1>Hello</h1>
 
       <form className="searchForm" onSubmit={handleSubmit}>
         <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search" type='text'/>
         <input className="submitSearchBtn btn" type='submit' value='Search'/>
+
+        <select className="selectDropDown">
+          <option>Select</option>
+          <option onClick={() => setHealthLabels('vegan')}>Vegan</option>
+          <option onClick={() => setHealthLabels('vegetarian')}>Vegetarian</option>
+          <option onClick={() => setHealthLabels('paleo')}>Paleo</option>
+          <option onClick={() => setHealthLabels('dairy-free')}>Dairy free</option>
+          <option onClick={() => setHealthLabels('gluten-free')}>Gluten free</option>
+          <option onClick={() => setHealthLabels('wheat-free')}>Wheat free</option>
+          <option onClick={() => setHealthLabels('low-sugar')}>Low sugar</option>
+          <option onClick={() => setHealthLabels('egg-free')}>Egg free</option>
+          <option onClick={() => setHealthLabels('peanut-free')}>Peanut free</option>
+          <option onClick={() => setHealthLabels('tree-nut-free')}>Tree nut free</option>
+          <option onClick={() => setHealthLabels('soy-free')}>Soy free</option>
+          <option onClick={() => setHealthLabels('fish-free')}>Fish free</option>
+          <option onClick={() => setHealthLabels('shellfish-free')}>Shellfish free</option>
+        </select>
       </form>
 
       <div>
         {recipes.map((recipe) => {
-          return <Card recipe={recipe}/>
+          return <Card key={uuidv4()} recipe={recipe}/>
         })}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Card({recipe}) {
   return (
+
     <div className='card'>
       <img className='cardImages' alt='recipe images' src={recipe['recipe']['image']} />
       <p className='recipeInfo'>{recipe['recipe']['label']}</p>
     </div>
+    
   )
 }
 
