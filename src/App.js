77176c932f98a7d4b0bcddb8726f79001d06f859
Jenna <jenna48071@gmail.com>
2022-09-17 21:50:09 -0500

    started mobile responsive

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Card from './components/Card'
 function App() {
   const [search, setSearch] = useState('');
   const [recipes, setRecipes] = useState([]);
-  const [healthLabels, setHealthLabels] = useState('alcohol-free');
 
   //https://api.edamam.com/search?q=chicken&app_id=1f947df2&app_key=bf2f0ead683c3918124966e995be981c&from=0&to=3&calories=591-722&health=alcohol-free
 
@@ -14,9 +13,9 @@ function App() {
   const APP_ID = '1f947df2'
 
   async function fetchData(){
-    let res = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${API_KEY}&health=${healthLabels}`)
+    let res = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${API_KEY}`)
     setRecipes(res.data.hits)
-    console.log(res.data);
+    console.log(res.data.hits);
   }
 
   const handleSubmit = (e) => {
@@ -28,33 +27,21 @@ function App() {
   return (
     <div className="App">
 
-      <form className="searchForm" onSubmit={handleSubmit}>
-        <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search" type='text'/>
-        <input className="submitSearchBtn btn" type='submit' value='Search'/>
+      <nav className="navContainer">
+        {/* Search  */}
+        <form className="searchForm" onSubmit={handleSubmit}>
+          <input className="searchInput" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Enter Recipe" type='text'/>
+          <input className="submitSearchBtn btn" type='submit' value='Search'/>
+        </form>
+      </nav>
 
-        <select className="selectDropDown">
-          <option>Select</option>
-          <option onClick={() => setHealthLabels('vegan')}>Vegan</option>
-          <option onClick={() => setHealthLabels('vegetarian')}>Vegetarian</option>
-          <option onClick={() => setHealthLabels('paleo')}>Paleo</option>
-          <option onClick={() => setHealthLabels('dairy-free')}>Dairy free</option>
-          <option onClick={() => setHealthLabels('gluten-free')}>Gluten free</option>
-          <option onClick={() => setHealthLabels('wheat-free')}>Wheat free</option>
-          <option onClick={() => setHealthLabels('low-sugar')}>Low sugar</option>
-          <option onClick={() => setHealthLabels('egg-free')}>Egg free</option>
-          <option onClick={() => setHealthLabels('peanut-free')}>Peanut free</option>
-          <option onClick={() => setHealthLabels('tree-nut-free')}>Tree nut free</option>
-          <option onClick={() => setHealthLabels('soy-free')}>Soy free</option>
-          <option onClick={() => setHealthLabels('fish-free')}>Fish free</option>
-          <option onClick={() => setHealthLabels('shellfish-free')}>Shellfish free</option>
-        </select>
-      </form>
-
-      <div>
+      {/* Display searched recipes */}
+      <div className="cardContainer">
         {recipes.map((recipe) => {
           return <Card key={uuidv4()} recipe={recipe}/>
         })}
       </div>
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Card({recipe}) {
   return (
-
     <div className='card'>
-      <img className='cardImages' alt='recipe images' src={recipe['recipe']['image']} />
-      <p className='recipeInfo'>{recipe['recipe']['label']}</p>
-    </div>
-    
+      <img className='cardImages' alt='recipe images' src={recipe.recipe.image} />
+      <p className='recipeInfo'>{recipe.recipe.label}</p>
+
+      {/* On-click route page to new */}
+      <button>See Full Recipe</button>
+    </div> 
   )
 }
 
