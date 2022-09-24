359d82ba6efad9cc4d9228d44f52b24572f1466c
Jenna <jenna48071@gmail.com>
2022-09-24 17:02:55 -0500

    updated cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,23 +5,31 @@ function Card({recipe}) {
   let calories = recipe.recipe.calories
   return (
     <div className='card'>
+
+      {/* IMAGE / RECIPE INFO */}
       <div className='ingredientsContainer'>
         <img className='cardImages' alt='recipe images' src={recipe.recipe.image} />
         <div className='ingredients'>{recipe.recipe.ingredientLines}</div>
-
       </div>
+
+      {/* NAME / MORE INFO */}
       <div className='cardInfoContainer'>
+
         <div className='cardInfo'>
-          <p className='recipeInfo'>{recipe.recipe.label}</p>
+          <h1 className='recipeInfo'>{recipe.recipe.label}</h1>
           <p className='recipeInfo'>{calories.toFixed()} cal</p>
+          <p className='recipeInfo'>{recipe.recipe.mealType}</p>
+          <p className='recipeInfo'>{recipe.recipe.yield} Servings</p>
         </div>
 
-        {/* On-click route page to new */}
+        {/* DIRECT TO FULL RECIPE PATH */}
         <button className='fullRecipeBtn btn'> 
           <Link to={`/recipe/${recipe.recipe.label}`} state={{data: recipe}}>Full Recipe</Link>
         </button>
 
       </div>
+
+
     </div> 
   )
 }
