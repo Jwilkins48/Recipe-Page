af60ae87cb4af22b6c52a2916bd6637092430669
Jenna <jenna48071@gmail.com>
2022-09-24 21:18:20 -0500

    adding dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,13 @@ function Header({search, setSearch, handleSubmit}) {
         <form className="searchForm" type='submit' onSubmit={handleSubmit}>
           <input className="searchInput" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Enter Recipe" type='text'/>
           <input className="searchLink btn" type='submit' value='Search'/>
+
+
         </form>
 
         <div className='headerRight'>
-        <Link to='/'><i class="fa-solid fa-seedling logo"></i></Link> 
-          </div>
+          <Link to='/'><i class="fa-solid fa-seedling logo"></i></Link> 
+        </div>
 
       </nav>
   )
